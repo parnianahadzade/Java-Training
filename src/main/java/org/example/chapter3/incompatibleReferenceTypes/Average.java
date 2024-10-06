6b82d0726ea3160e66c7266bc328cd7bb5d3dfdd
kurromiii <ahadzadeparnian@gmail.com>
2024-10-06 21:05:41 +0330

    sentinelControlledIteration

--- src/main/java/org/example/chapter3/incompatibleReferenceTypes/Average.java
+++ src/main/java/org/example/chapter3/incompatibleReferenceTypes/Average.java
@@ -6,6 +6,15 @@ public class Average {
     public static void main(String[] args) {
 
         //counter controlled iteration
+//        counterControlledIteration();
+
+        //sentinel controlled iteration
+        sentinelControlledIteration();
+
+        
+    }
+
+    public static void counterControlledIteration() {
         Scanner input = new Scanner(System.in);
         int total = 0;
         int count = 1;
@@ -18,6 +27,30 @@ public class Average {
 
         double average = (double) total / count;
         System.out.println("The average is " + average);
-        
+    }
+
+    public static void sentinelControlledIteration() {
+        int total = 0;
+        int count = 0;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your grade or -1 to exit: ");
+        int grade = input.nextInt();
+
+        while (grade != -1) {
+            total = total + grade;
+            count = count + 1;
+
+            System.out.println("Enter your grade or -1 to exit: ");
+            grade = input.nextInt();
+        }
+
+        if (count != 0) {
+            double average = (double) total / count;
+            System.out.println("The count is " + count);
+            System.out.println("The average is " + average);
+        }
+
+
     }
 }
